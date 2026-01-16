e28a628ea7b8d1a2d4a1b03c8a7d9aa1a12db4f6
taeo201 <taeo201@exeter.ac.uk>
2026-01-16 13:23:23 +0000

    game3

--- game_functions.py
+++ game_functions.py
@@ -20,4 +20,12 @@ def check_higher_lower(current_val, next_val, user_input):
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    if letter not in word:
+        print(f"Sorry, '{letter}' is not in the word")
+        return False
+    for i in range(len(word)):
+        if word[i] == letter:
+            board[i] = letter
+    print("Well done! '{}' is in the word")
+    return True
+        
